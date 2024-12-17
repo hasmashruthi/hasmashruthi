986e64fd0934ae7deb089ad8cb1639b0800a1dca
hasmashruthi <pradhiip92@gmail.com>
2024-12-17 12:07:39 +0530

    updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,44 +8,31 @@ function Header() {
 
   const [toggleMenu,setToggleMenu]=useState(false)
   return (
-    <header className="flex items-center justify-between px-5 py-2 bg-black">
-  {/* Left Section: Icon and Name */}
-  <div className="text-white text-1xl flex items-center px-5 font-bold">
-    <FaClipboard className="mr-2" /> {/* FontAwesome icon with margin */}
-    Shruthi
-  </div>
+    <header className=" flex justify-between  px-5 py-2 bg-black"> 
+      <div className="text-white text-2xl px-10  flex items-center"> 
+      <FaClipboard className="mr-2" /> {/* FontAwesome icon with margin */}
+      Shruthi
+    </div>
 
-  {/* Center Section: Navigation */}
-  <nav className="hidden md:block absolute left-1/2 transform -translate-x-1/2">
-    <ul className="flex space-x-8 text-white font-bold">
-      <li><a className="hover:text-gray-600" href="/">Home</a></li>
-      <li><a className="hover:text-gray-600" href="#about">About</a></li>
-      <li><a className="hover:text-gray-600" href="#projects">Projects</a></li>
-      <li><a className="hover:text-gray-600" href="#contact">Contact</a></li>
-    </ul>
-  </nav>
-
-  {/* Mobile Menu Button */}
-  <button
-    onClick={() => setToggleMenu(!toggleMenu)}
-    className="block md:hidden text-white"
-  >
-    <Bars3Icon className="h-5" />
-  </button>
-
-  {/* Mobile Menu */}
-  {toggleMenu && (
-    <nav className="block md:hidden absolute top-0 left-0 w-full bg-black bg-opacity-90">
-      <ul onClick={() => setToggleMenu(!toggleMenu)} className="flex flex-col text-white p-5">
-        <li><a className="hover:text-gray-400" href="/">Home</a></li>
-        <li><a className="hover:text-gray-400" href="#about">About</a></li>
-        <li><a className="hover:text-gray-400" href="#projects">Projects</a></li>
-        <li><a className="hover:text-gray-400" href="#contact">Contact</a></li>
-      </ul>
-    </nav>
-  )}
-</header>
-
+        
+            <nav className='hidden md:block'> 
+            <ul className='flex space-x-2 text-white font-bold'>
+            <li ><a className='hover:text-gray-600' href='/'>Home</a></li>
+            <li ><a className='hover:text-gray-600' href='#about'>About</a></li>
+            <li ><a className='hover:text-gray-600' href='#projects'>Projects</a></li>
+            <li ><a  className='hover:text-gray-600' href='#contact'>Contact</a></li>
+            </ul>
+            </nav>
+            {toggleMenu && <nav className='block md:hidden '> 
+            <ul onClick={()=>setToggleMenu(!toggleMenu)} className='flex flex-col text-white mobile-nav'>
+            <li ><a className='hover:text-gray-400' href='/'>Home</a></li>
+            <li ><a className='hover:text-gray-400'  href='#about'>About</a></li>
+            <li ><a className='hover:text-gray-400' href='#projects'>Projects</a></li>
+            <li ><a className='hover:text-gray-400' href='#contact'>Contact</a></li>
+            </ul>
+            </nav>}
+      <button onClick={()=>setToggleMenu(!toggleMenu)}className='block md:hidden'><Bars3Icon className='text-white h-5'/></button>
+    </header>
   )
 }
 
