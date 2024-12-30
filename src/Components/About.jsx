0e9e2fe49da36335ad141c7d9a0cc08f0e9d9412
hasmashruthi <pradhiip92@gmail.com>
2024-12-30 22:23:39 +0530

    new

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -55,7 +55,7 @@ function About() {
                 {/* Skills Content */}
                 <div className="flex items-center">
                   <label htmlFor="react" className="w-2/12">
-                    React
+                    React JS
                   </label>
                   <div className="grow bg-gray-800 rounded-full h-2.5">
                     <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-11/12"></div>
